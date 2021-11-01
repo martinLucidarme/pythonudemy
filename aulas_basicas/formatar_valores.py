be2acda1975964188e2214d92cb1699c91b49b86
martinLucidarme <martinlucidarme@gmail.com>
2021-11-01 14:33:04 -0300

    ref para formatar valores com funcao format no fstring

--- aulas_basicas/formatar_valores.py
+++ aulas_basicas/formatar_valores.py
@@ -11,6 +11,7 @@ Formatando valores:
 < - direita
 ^ - centro
 '''
+
 num_1 = 10
 num_2 = 3
 divisao = num_1 / num_2
@@ -23,5 +24,22 @@ print(f'{nome:s}')
 num1=49.514
 print(f'{num1:0>10}') #quero o num1 com 0 a ESQUERDA até preencher 10 casas
 
-num2=1150
-print(f'{num2:0<10}')
+num2 = 1150
+print(f'{num2:0>10.2f}') #combinação de fromatação
+
+nome2 = 'Martoni martin'
+sobrenome = 'Eba'
+print(f'{nome2:#^50}') #funciona tb com string
+
+nome_formatado = '{:@>25}'.format(nome2) # mesma coisa com function format
+nome_for = '{n:0>20}'.format(n=nome2) # formatando a chave n
+nome2_for = '{0:&>25} {1:#^25}'.format(nome2, sobrenome ) # formatando as variavel segundo lugar delas
+print(nome_formatado)
+print(nome_for)
+print(nome2_for)
+
+# Existe formatação de string inclusa no Python
+name = 'PiraCicaBa'
+print(name.lower())  # tudo minusc
+print(name.upper())  # tudo maiusc
+print(name.title())  # primeiras letras maiusculas
