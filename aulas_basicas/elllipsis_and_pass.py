c8b62c18a6c308087d27cce09c86461f65855fc1
martinLucidarme <martinlucidarme@gmail.com>
2021-11-01 12:06:04 -0300

    Exercicios 1 Udemy

---
+++ aulas_basicas/elllipsis_and_pass.py
@@ -0,0 +1,8 @@
+valor = False
+if valor:
+    # bloco vazio, ou comentarios -> da erro : expected indent
+    # por isso usa o pass : mostra que depois algo vai ser escrito
+    # ás vezes usa tb ellipsis: ...  não feito para isso mas tb funciona
+    pass
+else:
+    print('não nois')
---
+++ aulas_basicas/exercicios_1.py
@@ -0,0 +1,37 @@
+'''
+programa q peça digitar um nome inteiro, se nao for inteiro, digit: nao é inteiro
+'''
+
+numint = input('Digite um inteiro')
+if numint.isdigit():
+    print('Obrigado')
+else:
+    print('não é inteiro')
+
+'''
+Programa que pergunte hora e segundo hora responde a saudação apropriada
+'''
+hora = input('Digite a hora')
+minutos = input('Digite os minutos')
+hora_int = int(hora)
+if hora_int >= 0 and hora_int < 12:
+    print('Bom Dia !\nSão '+ hora +' horas e '+ minutos + ' minutos')
+elif hora_int >= 12 and hora_int<18:
+    print('Boa Tarde !\nSão ' + hora + ' horas e ' + minutos + ' minutos')
+else:
+    print('Boa Noite !\nSão ' + hora + ' horas e ' + minutos + ' minutos')
+
+'''
+Programa q peça nome do usuario, se for menor q 4 letras: print 'seu nome é curto", 
+entre 5 e 6 letras - normal, 
+maior que isso: muito grande
+'''
+
+nome = input('Digite seu nome')
+tam = len(nome)
+if tam <= 4:
+    print(f'Seu nome é curto, {nome} !')
+elif tam ==5 or tam ==6:
+    print(f'Seu nome é médio, {nome}')
+else:
+    print(f'Que nome grande ! {nome}')
