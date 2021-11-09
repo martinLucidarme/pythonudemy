2729b0c4b1a5c6c6e5073dff7078db5fea6456d6
martinLucidarme <martinlucidarme@gmail.com>
2021-11-09 12:21:13 -0300

    Lists basics + hangman game

--- aulas_basicas/listas.py
+++ aulas_basicas/listas.py
@@ -53,5 +53,50 @@ print(l2)
 del(l2[3:5])
 print(l2)
 
-# criar uma lista facilmente:
-# list_1_to_9 = list(range(1,10, step = 1))
+'''
+Aparte
+ criar uma lista facilmente:
+ list_1_to_9 = list(range(1,10, step = 1))
+ pode iterar em listas:
+
+l4 = [1,2,3,4,5,6,7,8,9]
+for valor in l4:
+    print(valor)
+'''
+
+# Exemplo do jogo adivinha palavra
+
+secreto = 'perfume'
+digitadas = [] # lista vazia
+chances = 3
+while True:
+
+    if chances < 0 :   # condição de derrota
+        print('Perdeu vacilão')
+        break
+
+    letra = input('Digite ume letra: ')
+    if len(letra)> 1:
+        print(' Tem que ser só uma letra rapai ')
+        continue
+    digitadas.append(letra) # armazena as letras digitadas enquanto o jogo dura
+
+    if letra in secreto:
+        print(f'Eba, {letra} existe na plavra secreta')
+    else:
+        digitadas.pop()
+        print(f'Vacilou, {letra} não existe na palavra secreta \n sobra {chances} chances')
+        chances -= 1
+
+    secreto_temporario = '' # esvazia a lista do print a cada laço
+    for letra_secreta in secreto: #checagem das letras da palavra uma por uma na ordem
+        if letra_secreta in digitadas: # a letra foi digitada pelo usuario
+            secreto_temporario += letra_secreta # adiciona na lista do PRINT
+        else:
+            secreto_temporario += '*'
+
+    if secreto_temporario == secreto: #condição de vitoria
+        print(f'Parabéns, você ganhou um {secreto}, pois a palavra era {secreto}')
+        break
+
+    print(secreto_temporario) # lista de ajuda do print, não de armazenamento (digitadas)
