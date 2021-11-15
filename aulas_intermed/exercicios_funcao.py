c086cbe60b7f0ddc9b1532fe22986af73667540c
martinLucidarme <martinlucidarme@gmail.com>
2021-11-15 11:22:08 -0300

    Basic exercises for function

---
+++ aulas_intermed/exercicios_funcao.py
@@ -0,0 +1,16 @@
+"""
+1 - Crie uma função saudação com paramêtro saudação e nome
+"""
+
+"""
+2 - Crie uma função que recebe 3 num e soma entre eles
+"""
+"""
+3 - crie uma função que recebe 2 num, um valor, um percentual
+return o valor do primeiro + aumento percentual dele
+"""
+"""
+4 - Fizz Buzz - Se o parametro da funcao for divisivel por 2
+return fizz, se for por 5 return buzz, se for por 5 e 3 return FizzBuzz
+contrario: return numero enviado
+"""
