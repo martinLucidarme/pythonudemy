4d4532f6560cdd7bbe1a65c731e1294d8961ec1a
martinLucidarme <martinlucidarme@gmail.com>
2021-11-19 16:22:19 -0300

    zip and zip_longest

--- aulas_intermed/zip_e_zip_longest.py
+++ aulas_intermed/zip_e_zip_longest.py
@@ -10,7 +10,7 @@ indice = count()  # gerador infinito que conta: cuidado com zip longest
 cidades = ['São Paulo', 'Belo Horizonte', 'Salvador', 'Rio de Janeiro', 'Monte Belo', 'Outra']
 estados = ['SP', 'MG', 'BA']
 
-cidades_estados = zip(cidades, estados)  # cria um gerador
+cidades_estados = zip(cidades, estados)  # cria um iterador
 cidades_estados2 = zip(estados, cidades, indice)  # pode zipar mais de 2
 print(cidades_estados)
 print(list(cidades_estados2))
