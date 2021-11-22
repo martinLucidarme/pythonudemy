cb88c987aa660ee17dd01e6c95eb746d95f3ee62
martinLucidarme <martinlucidarme@gmail.com>
2021-11-22 11:53:37 -0300

    2 dict, one list

---
+++ aulas_intermed/dados.py
@@ -0,0 +1,27 @@
+produtos = [
+    {'nome': 'p1', 'preco': 13},
+    {'nome': 'p1', 'preco': 55.55},
+    {'nome': 'p1', 'preco': 5.59},
+    {'nome': 'p1', 'preco': 81.23},
+    {'nome': 'p1', 'preco': 5.7},
+    {'nome': 'p1', 'preco': 10.9},
+    {'nome': 'p1', 'preco': 89.82},
+    {'nome': 'p1', 'preco': 12},
+    {'nome': 'p1', 'preco': 2.9},
+    {'nome': 'p1', 'preco': 19.99},
+]
+
+pessoas = [
+    {'nome':'Luiz', 'idade': 12},
+    {'nome': 'Eduarda', 'idade': 43},
+    {'nome': 'Joana', 'idade': 65},
+    {'nome': 'Lucas', 'idade': 19},
+    {'nome': 'Felipe', 'idade': 25},
+    {'nome': 'Maria', 'idade': 32},
+    {'nome': 'Júlia', 'idade': 23},
+    {'nome': 'Diniz', 'idade': 59},
+    {'nome': 'Max', 'idade': 17},
+    {'nome': 'Eduardo', 'idade': 32},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
