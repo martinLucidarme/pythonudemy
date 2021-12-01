7e0f512698404b9f43c9af85c4d63d4b08ae8199
martinLucidarme <martinlucidarme@gmail.com>
2021-12-01 14:20:43 -0300

    Open and modify files - basics (txt and json)

--- aulas_intermed/aulas_open_write_files/abrir_ler_arquivos_part2.py
+++ aulas_intermed/aulas_open_write_files/abrir_ler_arquivos_part2.py
@@ -2,6 +2,10 @@
 https://www.docs.python.org/3/library/functions.html#open
 '''
 
+
+import os
+import json
+
 """
 try:  # commum usar try para abrir arquivos, porém, nao muito Pythonico
     file = open('abc.txt', 'w+')
@@ -23,4 +27,36 @@ with open('abc2.txt', 'w+') as file:  # melhor maneira no Python
 
 # outros modulos:
 # open('file', 'a+') não apaga o arquivo: adiciona linhas
+
+file2 = open('file2.txt','a+')
+file2.write('outra linha sem apagar\n')
+file2.write('outra linha sem apagar\n')
+file2.close()
+
+os.remove('file2.txt')  # pode deletar o arquivo
+
 # open('file', 'r') so para ler o arquivo
+
+# dict e json:
+
+d1 = {
+    'Pessoa 1': {
+        'nome':'Luiz',
+        'idade':25,
+    },
+    'Pessoa 2': {
+        'nome':'Rose',
+        'idade': 26,
+    },
+}
+
+d1_json = json.dumps(d1, indent=True)  # passa o dico para json
+
+with open('arquivo.json', 'w+') as djson:
+    djson.write(d1_json)
+    d2_json = json.loads(d1_json)
+
+for k, v in d2_json.items():
+    print(k)
+    for k1, v1 in v.items():
+        print(k1,v1)
