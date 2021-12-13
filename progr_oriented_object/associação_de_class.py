474c8ffa8649cd9b4cf7e8b208960f643414f0ff
martinLucidarme <martinlucidarme@gmail.com>
2021-12-13 15:02:20 -0300

    getters and setters part 2

---
+++ progr_oriented_object/associação_de_class.py
@@ -0,0 +1,17 @@
+from classes import Escritor, Caneta, MaquinaDeEscrever
+
+escritor = Escritor('Joazim')
+caneta = Caneta('Bic')
+maquina = MaquinaDeEscrever()
+
+print(escritor.nome)
+print(caneta.marca)
+maquina.escrever()
+print()
+print('----- Associação de class: ------')
+escritor.ferramenta = maquina  # associando a ferramenta ao escritor
+escritor.ferramenta.escrever()
+
+del escritor
+#print(escritor)  # escritor deixou de existir -> vai dar erro
+print(caneta.marca)  # caneta (e maquina) ainda existe: associação 'fraca'
