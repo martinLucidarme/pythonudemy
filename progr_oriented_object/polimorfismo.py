291e763bc69aafd210514fbc6fae5aa29158e990
martinLucidarme <martinlucidarme@gmail.com>
2021-12-16 16:40:39 -0300

    polymorphism in Python (overrides only)

---
+++ progr_oriented_object/polimorfismo.py
@@ -0,0 +1,30 @@
+"""
+Polimorfismo é o principio que permite que classes derivadas de uma mesma
+superclasse tenham métodos iguais (de mesma assinatura) mas comportamentos
+diferentes.
+mesma assinatura = mesma qtd e tipo de parametros
+"""
+from abc import ABC, abstractmethod
+
+
+class A(ABC):
+    @abstractmethod
+    def fala(self, msg):
+        pass
+
+
+class B(A):
+    def fala(self, msg):
+        print(f'B está falando {msg}')
+
+
+class C(A):
+    def fala(self, msg):
+        print(f'C está falando {msg}')
+
+
+b = B()
+c = C()
+b.fala('Um assunto')
+c.fala('Outro assunto')  # polimorfismo de sobreposicao: o unico em Python
+
