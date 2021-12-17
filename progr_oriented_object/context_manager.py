05d6d314cb3b878b783970f92c3a42591d9d0c9a
martinLucidarme <martinlucidarme@gmail.com>
2021-12-17 14:39:14 -0300

    few examples and useful ink for magic methods

---
+++ progr_oriented_object/context_manager.py
@@ -0,0 +1,48 @@
+'''
+exemplo
+arquivo = open('abc.txt', 'w')
+arquivo.write('Alguma coisa')
+arquivo.close()  # muitas vezes: esquece de fechar => usa um context manager
+'''
+
+from contextlib import contextmanager
+
+"""
+método sem importar nada
+class Arquivo:
+    def __init__(self, arquivo, modo):
+        print('INIT')
+        self.arquivo = open(arquivo, modo)
+
+    def __enter__(self):
+        print('Entrou na classe')
+        return self.arquivo
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Saiu da classe')
+        self.arquivo.close()
+
+
+with  Arquivo('abc.txt', 'w') as arquivo:
+    arquivo.write('Alguma coisa')
+
+"""
+
+
+# OUTRA MANEIRA
+
+@contextmanager
+def abrir(arquivo, modo):
+    try:
+        print('Abrindo arquivo')
+        arquivo = open(arquivo, modo)
+        yield arquivo
+    finally:
+        print('Fechando arquivo')
+        arquivo.close()
+
+
+with abrir('abc2.txt', 'w') as file:
+    file.write('Linha 1\n')
+    file.write('Linha 2\n')
+    file.write('Linha 3\n')
