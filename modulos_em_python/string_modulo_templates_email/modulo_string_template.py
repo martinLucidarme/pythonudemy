fe2dc35c646ae8a37022df532d03b5e80e88fd54
martinLucidarme <martinlucidarme@gmail.com>
2022-01-11 17:04:34 -0300

    How tu use string module to create html template

---
+++ modulos_em_python/string_modulo_templates_email/modulo_string_template.py
@@ -0,0 +1,11 @@
+# para parametrar a pagina html, digit html na pagina e ctrl+'espaço'
+
+from string import Template
+from datetime import datetime
+
+with open('template.html', 'r') as html:
+    template = Template(html.read())
+    data_atual = datetime.now().strftime('%d/%m/%Y')
+    corpo_msg = template.substitute(nome='Martin', data=data_atual)
+
+print(corpo_msg)
