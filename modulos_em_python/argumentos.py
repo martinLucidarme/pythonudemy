f96780fa75202d538204e97142f75049164d2f95
martinLucidarme <martinlucidarme@gmail.com>
2022-02-01 15:56:35 -0300

    use arguments with sys,  start a script out of Pycharm

---
+++ modulos_em_python/argumentos.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+argumentos = sys.argv
+qtd_argumentos = len(argumentos)
+
+if qtd_argumentos <= 1:
+    print('faltando argumentos')
+    print('-a', 'Para listar todos os arquivos nessa pasta', sep='\t')
+    print('-d', 'Para listar todos os diretorios nessa pasta', sep='\t')
+    sys.exit()
+
+so_arquivos = False
+if '-a' in argumentos:
+    so_arquivos = True
+
+so_diretorios = False
+if '-d' in argumentos:
+    so_diretorios = True
+
+for arquivo in os.listdir('.'):
+    if so_arquivos:
+        if os.path.isfile(arquivo):
+            print(arquivo)
+
+    if so_diretorios:
+        if os.path.isdir(arquivo):
+            print(arquivo)
