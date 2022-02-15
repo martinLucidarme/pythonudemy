c2632dd4914d97a65713025ff0c97874581c8f37
martinLucidarme <martinlucidarme@gmail.com>
2022-02-15 15:30:03 -0300

    Convert videos in Python

---
+++ modulos_em_python/modulo_jupyter.py
@@ -0,0 +1,7 @@
+"""
+jupyter Notebook é uma app web que permite execução de codigo
+em tempo real, visu de dados e txts explicativos
+pode criar/executar codigo e explicar enquanto mostram os dados
+
+pip install jupyter
+"""
