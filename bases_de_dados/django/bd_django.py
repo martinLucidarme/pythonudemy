20d998496b75475a9d2775194fc7f99e6ff9189a
martinLucidarme <martinlucidarme@gmail.com>
2022-05-10 13:33:52 -0300

    Base de dados com django

---
+++ bases_de_dados/django/bd_django.py
@@ -0,0 +1,11 @@
+""""
+Ferramenta essencial para backend
+terminal commad:
+pip install django  # instala django
+django-admin startproject projeto_django .   # cria projeto
+
+cf projeto django (outro projeto)
+"""
+
+
+
